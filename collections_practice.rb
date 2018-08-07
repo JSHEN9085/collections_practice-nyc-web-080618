db7230756991902ea5a9e82fdbf233b14faf8735
JUN JIAN SHEN <jshen9085@gmail.com>
2018-08-07 01:36:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b }
+end 
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end 
+
+def sort_array_char_count(array  )
+array.sort_by {|x| x.length}
+
+# or array.sort {|x, y| x.length <=> y.length}
+
+end 
+
+def swap_elements(array)
+ new_arr = []
+ new_arr.push(array[0])
+ new_arr.push(array[2])
+ new_arr.push(array[1])
+ return new_arr
+end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end 
+  array
+end 
+
+def find_a(array)
+  
+end 
