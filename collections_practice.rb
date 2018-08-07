dd5a6aaeaca0b9645450a0efb78c6f9dee88a462
JUN JIAN SHEN <jshen9085@gmail.com>
2018-08-07 01:50:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| if index!== }
+  array.each_with_index.collect {|element, index| if index!== 2 }
+  
 end 
